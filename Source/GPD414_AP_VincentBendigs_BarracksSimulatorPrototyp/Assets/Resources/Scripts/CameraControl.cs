2c73582f19c69e9e5905385e64336ac3f8b6af3b
Lether1983 <VincentBendigs@googlemail.com>
2015-05-13 08:01:17 +0200

    Added: The CameraControl Sctipt

---
+++ Source/GPD414_AP_VincentBendigs_BarracksSimulatorPrototyp/Assets/Resources/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraControl : MonoBehaviour 
+{
+    public Vector2 moveDirection;
+    public float Speed;
+    TileMap map = TileMap.Instance();
+   
+
+	// Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal")*Speed, Input.GetAxis("Vertical")*Speed);
+
+        transform.Translate(moveDirection * Time.deltaTime);
+        transform.position = new Vector2(Mathf.Clamp(this.transform.position.x, (float)(map.purchasedLandWidthMin), (float)(map.purchasedLandWidthMax)),
+                                         Mathf.Clamp(this.transform.position.y, (float)(map.purchasedLandHeightMin), (float)(map.purchasedLandHeightMax)));
+    }
+}
