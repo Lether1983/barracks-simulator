65adafb726c3e2147696161bab93595a22c573b8
Lether1983 <VincentBendigs@googlemail.com>
2015-05-13 17:36:59 +0200

    Added: Menu Controller for the UI

---
+++ Source/GPD414_AP_VincentBendigs_BarracksSimulatorPrototyp/Assets/Resources/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour 
+{
+    public GameObject BauPanel;
+    public GameObject TilePanel;
+    void Start()
+    {
+        BauPanel.SetActive(false);
+        TilePanel.SetActive(false);
+    }
+
+    public void BauMenuPanelActivate()
+    {
+       if(BauPanel.activeInHierarchy)
+       {
+           BauPanel.SetActive(false);
+       }
+       else
+       {
+           BauPanel.SetActive(true);
+       }
+    }
+    public void TileMenuPanelActivate()
+    {
+        if(TilePanel.activeInHierarchy)
+        {
+            TilePanel.SetActive(false);
+        }
+        else
+        {
+            TilePanel.SetActive(true);
+        }
+    }
+}
