41b9b0fc56fe3df6186df2f48f7347677b51c013
Lether1983 <VincentBendigs@googlemail.com>
2015-05-30 14:20:44 +0200

    Updated: The RoomMap for Generation

--- Source/GPD414_AP_VincentBendigs_BarracksSimulatorPrototyp/Assets/Resources/Scripts/RoomMap.cs
+++ Source/GPD414_AP_VincentBendigs_BarracksSimulatorPrototyp/Assets/Resources/Scripts/RoomMap.cs
@@ -7,16 +7,18 @@ public class RoomMap
 
     public RoomBaseClass[,] RoomData;
 
-    public void GenerateRoomObjects(int Mapsize)
+    public void GenerateRoomObjects(int MapSize)
     {
-        for (int i = 0; i < Mapsize ; i++)
+        RoomData = new RoomBaseClass[MapSize, MapSize];
+        for (int i = 0; i < MapSize; i++)
         {
-            for (int j = 0; j < Mapsize; j++)
+            for (int j = 0; j < MapSize; j++)
             {
                 RoomData[i, j] = new RoomBaseClass();
             }
         }
     }
+
     public static RoomMap Instance()
     {
         if (instance == null)
